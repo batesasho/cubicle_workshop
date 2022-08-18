9b0884d326d340b527203fd8739431555b13e229
batesasho <agenov@gmail.com>
2022-08-18 12:25:40 +0300

    added handlebars and home page rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+const handlebars = require('express-handlebars');
 
 const app = express();
 
+app.use("/static",express.static('public'));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views','./src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index');
 });
 
 
